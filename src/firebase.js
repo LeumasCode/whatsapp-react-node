d844102087dc7ca7c70b331d6ab0f518a8d5edc8
Damilare <opeyemisamuel222@gmail.com>
2021-01-30 15:46:25 +0000

    starting with sidebar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLxB9L6OHMJWV2CWcXLS-P0Pff8TKZb98",
+  authDomain: "whatsapp-react-node.firebaseapp.com",
+  projectId: "whatsapp-react-node",
+  storageBucket: "whatsapp-react-node.appspot.com",
+  messagingSenderId: "46237074785",
+  appId: "1:46237074785:web:6ff626528548bb5f417120",
+};
