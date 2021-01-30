000162a8e7d5fb53e2742d0e3a70cdfa0d275187
Damilare <opeyemisamuel222@gmail.com>
2021-01-30 17:19:30 +0000

    done with the sidebar header

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVert from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            I am sidebar
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src='' />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
         </div>
-    )
-}
+      </div>
 
-export default Sidebar
+      <div className="sidebar__search">
+          <div className="sidebar__searchContainer">
+                <SearchOutlined/>
+                <input type="text" placeholder='Search a chat or start a new chat'/>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
