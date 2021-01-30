0fec15dc34666502561defd00b656ca86c1902e2
Damilare <opeyemisamuel222@gmail.com>
2021-01-30 18:35:08 +0000

    top header of the chatScreen done

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import './Chat.css'
+import { Avatar, IconButton } from "@material-ui/core";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import React from "react";
+import "./Chat.css";
 
 const Chat = () => {
-    return (
-        <div className='chat'>
-            I am a chat
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src="" />
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>last seen at ...</p>
         </div>
-    )
-}
 
-export default Chat
+        <div className="chatHeaderRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
