020ab11e582a73e30dcdc5695df8487d1d965a63
Damilare <opeyemisamuel222@gmail.com>
2021-01-30 20:04:27 +0000

    ui done

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
 import React from "react";
 import "./Chat.css";
 
 const Chat = () => {
+  const [input, setInput] = useState("");
+
+const sendMessage = () => {
+    console.log('sent');
+}
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -38,11 +51,22 @@ const Chat = () => {
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
         </p>
-
-     
       </div>
 
-      
+      <div className="chat__footer">
+        <InsertEmoticon />
+
+        <form>
+          <input
+            value={input}
+            type="text"
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={sendMessage}>Send a message</button>
+        </form>
+
+        <Mic />
+      </div>
     </div>
   );
 };
